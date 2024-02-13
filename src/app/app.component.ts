032f16bf15164c71a6a91fc7e00cf7c4e72b57fd
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2024-02-13 19:47:52 -0300

    build project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AngularFullpageModule } from '@fullpage/angular-fullpage';
 // components
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -14,7 +13,6 @@ import { LoadingComponent } from './components/loading/loading.component';
     FooterComponent,
     LoadingComponent,
     RouterOutlet,
-    AngularFullpageModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
