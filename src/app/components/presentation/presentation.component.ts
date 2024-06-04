d1c3b704eb29f009e0bb17138addedd762a7319f
brunocarvalhs <bruno.silva.carvalho@outlook.com.br>
2024-06-04 10:18:21 -0300

    imagem repositori base github

--- src/app/components/presentation/presentation.component.ts
+++ src/app/components/presentation/presentation.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { sections } from '../../app.sections';
 import Typed from 'typed.js';
+import { GithubService } from '../../services/github/github.service';
+import { throws } from 'assert';
 
 @Component({
   selector: 'app-presentation',
@@ -11,12 +13,20 @@ import Typed from 'typed.js';
   styleUrl: './presentation.component.scss',
 })
 export class PresentationComponent {
+
   sections = sections
   greeting = 'Hello!';
   fullName = 'Bruno Carvalho';
+  image = "/assets/images/presentation/presentation.png"
   professions = ['Mobile Android', 'Full Stack'];
 
+  constructor(private githubService: GithubService) {}
+
   ngOnInit(): void {
+    
+  }
+
+  featchData() {
     const options = {
       strings: this.professions.flatMap((value) => {
         return '<h2 class="fw-bold text-yellow">' + value + '</h2>';
@@ -29,5 +39,10 @@ export class PresentationComponent {
     };
 
     const typed = new Typed('.typed-text', options);
-  }
+
+    this.githubService.getUserProfile().subscribe((githubProfile) => {
+      this.fullName = githubProfile.name
+      this.image = githubProfile.avatar_url
+    })
+    }
 }
