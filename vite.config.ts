05ca2fb03048c3c79521a822eb141ec0f12d9ddd
Bruno <bruno.silva.carvalho@outlook.com.br>
2025-09-23 22:51:58 -0300

    feat: build do site

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig({
-  base: "/brunocarvalhs.github.io/",
+export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
   build: {
-    outDir: "docs",
+    outDir: "docs", 
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
